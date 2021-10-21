c730c57fcf7b25acf0c05461978292d0304dda8b
ula345 <krzeminska.ula@gmail.com>
2021-10-21 23:31:17 +0200

    added GitHub link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-    Weather app
+   <h1> Weather app</h1>
+<footer> This project was created by Urszula Krzemińska and is {" "}
+    <a href="https://github.com/ula345/weather-app-react.git" target="_blank"> open-sourrce on GitHub </a>
+    </footer>
     </div>
   );
 }
 
-export default App;
+
