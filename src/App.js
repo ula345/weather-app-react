e95b964a5a611777b8cad8016e1c76dcf14efad5
ula345 <krzeminska.ula@gmail.com>
2021-10-21 23:45:11 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
    <h1> Weather app</h1>
 <footer> This project was created by Urszula Krzemińska and is {" "}
-    <a href="https://github.com/ula345/weather-app-react.git" target="_blank"> open-sourrce on GitHub </a>
+    <a href="https://github.com/ula345/weather-app-react.git" target="_blank"> open-sourrce on GitHub </a> 
+    and is run by <a href="https://heuristic-goodall-dd8039.netlify.app/"target="_blank">Netlify</a> 
     </footer>
-    </div>
+    </div></div>
   );
 }
 
