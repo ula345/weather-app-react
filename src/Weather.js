7faaef65d256c3a29451006423edf7be14265997
ula345 <krzeminska.ula@gmail.com>
2021-10-26 20:08:28 +0200

    fahrenhei convertor

--- src/Weather.js
+++ src/Weather.js
@@ -1,67 +1,70 @@
 import React, {useState} from "react";
+import WeatherInfo from "./Weatherinfo.js";
 import axios from "axios";
 import "./Weather.css";
-import FormattedDate from "./FormattedDate";
 
-export default function Weather (props) { 
-  const [weatherData, setWeatherData] = useState({ready: false});
-function handleResponse(response) { 
-  setWeatherData({
-ready:true,
-temperature: response.data.main.temp,
-humidity: response.data.main.humidity,
-description: response.data.weather[0].description,
-wind: response.data.wind.speed,
-city: response.data.name,
-date: new Date (response.data.dt * 1000)
-});}
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-if (weatherData.ready) {
-    return( 
-        <div className="Weather"> 
-      <form>
-    <div className="row"> 
-             <div className="col-9">
-      <input type="search" placeholder="Enter a city.." className="formSubmit" autoFocus="on"/>
-      <div className="col-3"> 
-      <input type="Submit" className="btn btn-primary w-100" value="Search" />
-      </div>
-      </div> 
-      </div>
-    </form>
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      date: new Date(response.data.dt * 1000),
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
+
+function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
 
-   <h1> {weatherData.city}</h1>
-   <ul> 
-     <li> <FormattedDate date={weatherData.date}/> </li></ul>
-    
-<div className="row">
-    <div className="col-6"> 
-      <img src=
-      "https://cdn.iconscout.com/icon/premium/png-256-thumb/unknown-weather-4127335-3425639.png" alt="Partially sunny"/>
-    
-  <span className="temperature">{Math.round(weatherData.temperature)}</span> 
-  <span className="unit">°C</span>   </div>
-     <div className="col-6">  
-       <h2> {weatherData.date}</h2>
-        <ul>
-        
-            <li> {weatherData.description}</li>
-          <li> Humidity: {weatherData.humidity} %</li>
-          <li> Wind: {weatherData.wind} km/h</li>
-        </ul>
-      </div>
-    </div> 
-            </div>
-    );
-} 
-
-else {
-
+function search() { 
 const apiKey = "c3eaadab98311afcd7db046c4c4d8248";
-  let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`; 
+  let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`; 
   axios.get(apiUrl).then(handleResponse);
+}
 
+ if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city.."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+      </div>
+    );
+    } 
+    
+    else {
+search();
   return "Loading...";
 }
 }
---
+++ src/Weatherinfo.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Weather.css";
+import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WetaherInfo(props) { 
+    return (  
+         <div className="WeatherInfo"> 
+         <h1>{props.data.city}</h1>
+   <ul> 
+     <li> <FormattedDate date={props.data.date}/> </li></ul>
+    
+<div className="row">
+    <div className="col-6"> 
+      <img src=
+      "https://cdn.iconscout.com/icon/premium/png-256-thumb/unknown-weather-4127335-3425639.png" alt="Partially sunny"/>
+    
+    <WeatherTemperature celsius={props.data.temperature} /> </div>
+     <div className="col-6">  
+       <h2> {props.data.date}</h2>
+        <ul>
+        
+            <li> {props.data.description}</li>
+          <li> Humidity: {props.data.humidity} %</li>
+          <li> Wind: {props.data.wind} km/h</li>
+        </ul>
+      </div>
+    </div> 
+    </div>);
+}
