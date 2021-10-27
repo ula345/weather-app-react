5d5ce9c93a736e74032ed745f3fdc2d7987fbecc
ula345 <krzeminska.ula@gmail.com>
2021-10-28 00:53:26 +0200

    a href updated

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
       <div className="container">
    <Weather defaultCity = "London"/>
 <footer> This project was created by Urszula Krzemińska and is {" "}
-    <a href="https://github.com/ula345/weather-app-react.git" target="_blank"> open-sourrce on GitHub </a> 
-    and is run by <a href="https://heuristic-goodall-dd8039.netlify.app/"target="_blank">Netlify</a> 
+          <a href ="https://github.com/ula345/weather-app-react.git" rel="noreferrer" target="_blank"> open-sourrce on GitHub </a>
+    and is run by <a href ="https://heuristic-goodall-dd8039.netlify.app/" rel="noreferrer" target="_blank">Netlify</a> 
     </footer>
     </div></div>
   );
